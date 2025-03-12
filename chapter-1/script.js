3c64cbef9f1c931bb5987a62a224026b7d4b4a2c
Prashanth <mail2sprashanth@gmail.com>
2025-03-12 22:07:03 +0530

    first commit

---
+++ chapter-1/script.js
@@ -0,0 +1,19 @@
+alert("Good Morning");
+
+alert("Prashanth");
+
+console.log(10+5);
+
+console.log(20-5);
+
+console.log(2+2-5)
+
+document.body.innerHTML = "Good Morning!";
+
+document.body.innerHTML = "Prashanth";
+
+console.log(10 + 8 + 20);
+
+console.log(100 - 50 - 20 + 200);
+
+document.body.innerHTML = " ";
